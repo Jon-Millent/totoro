b2e4427f9d5433d88ed3f91074361da71ef510a4
millent <millent@126.com>
2020-07-08 10:20:49 +0800

    add todo list

--- desk/preload.js
+++ desk/preload.js
@@ -1,23 +1,20 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 
-
-const { writeFileSync, existsSync } = require('fs')
-const JImgHelper = require('./src/modules/JimgHelper')
-const iconPromise = require('icon-promise');
-const path = require('path');
-const shortid = require('shortid')
-const wallpaper = require('wallpaper');
-
 const baseUrl = __dirname
-
+const path = require('path');
 const accessDir = path.join(baseUrl, 'access')
-
 
 class Preload {
 
   // 获取exe图标
   static async getEXEIconFormPath(exeFilePath) {
+
+    const { writeFileSync, existsSync } = require('fs')
+    const JImgHelper = require('./src/modules/JimgHelper')
+    const iconPromise = require('icon-promise');
+    const shortid = require('shortid')
+
     if (!exeFilePath) {
       return null
     }
@@ -27,6 +24,8 @@ class Preload {
     }
 
     const fileName = shortid.generate()
+
+    // 这里因为有的应用没有大图标,且返回大图标的时候会有问题,这里就判断不支持大图标的返回小图标
     const largeInfo = await iconPromise.getIcon256(exeFilePath, fileName)
     const smallInfo = await iconPromise.getIcon48(exeFilePath, fileName)
 
@@ -53,11 +52,15 @@ class Preload {
 
   // 更换壁纸
   static async changeWallpaper(path) {
+    const wallpaper = require('wallpaper');
+
     return await wallpaper.set(path)
   }
 
   // 获取当前壁纸
   static async getWallpaper() {
+    const wallpaper = require('wallpaper');
+
     return await wallpaper.get()
   }
 
---
+++ dev.md
@@ -0,0 +1,27 @@
+
+## 电量&锁屏监控
+https://www.electronjs.org/docs/api/power-monitor
+
+## 储存
+```shell script
+electron-store
+```
+
+## 应用内快捷键监听
+https://www.electronjs.org/docs/api/accelerator
+
+## 剪贴板
+https://www.electronjs.org/docs/api/clipboard
+
+## 应用外快捷键监听
+https://www.electronjs.org/docs/api/global-shortcut
+
+## 本地化
+https://www.electronjs.org/docs/api/locales
+
+## 推送
+https://www.electronjs.org/docs/api/notification
+
+## [待定] windows下小程序功能 + app 商店
+用户可以自主开发提交应用
+
